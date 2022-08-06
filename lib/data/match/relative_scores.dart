6011e61b5f6f9aff07625c5b02234f0c604fc2aa
jslater89 <slater.jay@gmail.com>
2022-08-06 10:02:29 -0400

    Refactor 'model' into separate files, reexported to avoid other code changes

---
+++ lib/data/match/relative_scores.dart
@@ -0,0 +1,49 @@
+import 'package:collection/collection.dart';
+import 'package:uspsa_result_viewer/data/model.dart';
+import 'package:uspsa_result_viewer/data/ranking/rater.dart';
+
+class RelativeMatchScore {
+  RelativeMatchScore({required this.shooter});
+
+  Shooter shooter;
+  late RelativeScore total;
+
+  /// Percent of match max points shot
+  late double percentTotalPoints;
+
+  Map<Stage, RelativeScore> stageScores = {};
+}
+
+extension MatchScoresToCSV on List<RelativeMatchScore> {
+  String toCSV() {
+    String csv = "Member#,Name,MatchPoints,Percentage\n";
+    var sorted = this.sorted((a, b) => a.total.place.compareTo(b.total.place));
+
+    for(var score in sorted) {
+      csv += "${Rater.processMemberNumber(score.shooter.memberNumber)},";
+      csv += "${score.shooter.getName(suffixes: false)},";
+      csv += "${score.total.relativePoints.toStringAsFixed(2)},";
+      csv += "${(score.total.percent * 100).toStringAsFixed(2)}\n";
+    }
+
+    return csv;
+  }
+}
+
+class RelativeScore {
+  RelativeScore();
+  late Score score;
+
+  int place = -1;
+
+  /// If null, treat this as a match score
+  Stage? stage;
+
+  /// For stage scores, calculate this off of
+  /// hit factors and use to set relative points.
+  double percent = 0;
+
+  /// For match scores, total from stage scores
+  /// and use to set percent.
+  double relativePoints = 0;
+}
