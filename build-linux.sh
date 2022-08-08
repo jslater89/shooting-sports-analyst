f220061175b0154f72c356c5d521226aef026920
Jay Slater <jay@manywords.press>
2022-08-08 17:37:22 -0400

    Linux build script

---
+++ build-linux.sh
@@ -0,0 +1,15 @@
+if [ -z $1 ]; then
+    echo "Requires version number arg"
+    exit
+fi
+
+
+PROJ_ROOT=`pwd`
+flutter build linux
+cd build/linux/x64/release
+rm -rf uspsa-result-viewer
+cp -r bundle uspsa-result-viewer
+zip -r uspsa-result-viewer.zip uspsa-result-viewer
+cd $PROJ_ROOT
+cp build/linux/x64/release/uspsa-result-viewer.zip uspsa-result-viewer-$1-linux.zip
+
