3235b1b4c6453a39fd17b52771c433f935206d0a
Jay Slater <jay@manywords.press>
2022-09-12 10:18:43 -0400

    Files for macOS builds

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,12 +40,12 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "uspsa_result_viewer");
+    gtk_header_bar_set_title(header_bar, "USPSA Result Viewer");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else {
-    gtk_window_set_title(window, "uspsa_result_viewer");
+    gtk_window_set_title(window, "USPSA Result Viewer");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
