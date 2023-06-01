daa37cdfcd83c6109274be74996eb3b9944598c9
jslater89 <slater.jay@gmail.com>
2023-06-01 08:57:23 -0400

    Windows build script cleans up after itself better

--- build-windows.ps1
+++ build-windows.ps1
@@ -9,9 +9,12 @@ fvm flutter build windows
 
 $Root = Get-Location
 cd .\build\windows\runner
+rm -r -Force uspsa-result-viewer
+mkdir uspsa-result-viewer
 cp -r -Force .\Release\* uspsa-result-viewer
 cp $Root\data\L2s-Since-2019.json uspsa-result-viewer
 cp $Root\data\Nationals-and-Area-Matches.json uspsa-result-viewer
+rm uspsa-result-viewer.zip
 Compress-Archive -Path .\uspsa-result-viewer -DestinationPath uspsa-result-viewer.zip -Force
 cd $Root
 cp -Force .\build\windows\runner\uspsa-result-viewer.zip uspsa-result-viewer-$version-windows.zip
