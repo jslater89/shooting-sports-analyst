b18bf09450e594bf4d1554c58a373e8c974b20b0
Jay Slater <slater.jay@gmail.com>
2024-12-30 09:23:21 -0500

    MPL preamble

--- lib/data/ranking/model/json_shooter_rating.dart
+++ lib/data/ranking/model/json_shooter_rating.dart
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 import 'package:json_annotation/json_annotation.dart';
 import 'package:shooting_sports_analyst/data/ranking/model/shooter_rating.dart';
