714456bbfe8a143059ba84ad8e950a9c98a98702
Jay Slater <slater.jay@gmail.com>
2025-02-11 14:39:58 -0500

    License preambles

--- lib/ui/widget/dialog/help/help_registry.dart
+++ lib/ui/widget/dialog/help/help_registry.dart
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import 'package:shooting_sports_analyst/data/help/about.dart';
 import 'package:shooting_sports_analyst/data/help/deduplication_help.dart';
 import 'package:shooting_sports_analyst/data/help/elo_help.dart';
