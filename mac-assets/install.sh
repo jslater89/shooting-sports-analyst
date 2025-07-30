34a615376e7d5c5e36aa626a90fe23aae088ad56
Jay Slater <slater.jay@gmail.com>
2025-07-29 23:25:58 -0400

    Mac install script fix, maybe?

--- mac-assets/install.sh
+++ mac-assets/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cp -r "Shooting Sports Analyst.app" /Applications
+xattr -dr com.apple.quarantine "/Applications/Shooting Sports Analyst.app"
 spctl --add "/Applications/Shooting Sports Analyst.app"
