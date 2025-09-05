5f3b9a1c47f9f56491e9c7cbea9ee663a56f2e28
Jay Slater <jay@Jays-MacBook-Pro.local>
2025-09-05 10:50:59 -0400

    Use FSS/EFS-delegating storage provider in SecureConfig

--- lib/config/secure_config.dart
+++ lib/config/secure_config.dart
@@ -4,29 +4,31 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shooting_sports_analyst/main.dart';
 
 class SecureConfig {
   static const _psUsernameKey = "ps_username";
   static const _psPasswordKey = "ps_password";
 
+  static final _storage = FlutterSecureStorageProvider();
+
   static Future<void> setPsUsername(String username) async {
-    await FlutterSecureStorage().write(key: _psUsernameKey, value: username);
+    await _storage.write(_psUsernameKey, username);
   }
 
   static Future<void> setPsPassword(String password) async {
-    await FlutterSecureStorage().write(key: _psPasswordKey, value: password);
+    await _storage.write(_psPasswordKey, password);
   }
 
   static Future<(String?, String?)> getPsCredentials() async {
-    var username = await FlutterSecureStorage().read(key: _psUsernameKey);
-    var password = await FlutterSecureStorage().read(key: _psPasswordKey);
+    var username = await _storage.read(_psUsernameKey);
+    var password = await _storage.read(_psPasswordKey);
     return (username, password);
   }
 
   static Future<void> clearPsCredentials() async {
-    await FlutterSecureStorage().delete(key: _psUsernameKey);
-    await FlutterSecureStorage().delete(key: _psPasswordKey);
+    await _storage.delete(_psUsernameKey);
+    await _storage.delete(_psPasswordKey);
   }
 }
 
