dfac8ef19d7777ca902dd691dde1c4592d9a4353
Jay Slater <jay@Jays-MacBook-Pro.local>
2025-09-04 20:28:46 -0400

    Improved encrypted file store

--- lib/config/encrypted_file_store/macos_key_deriver.dart
+++ lib/config/encrypted_file_store/macos_key_deriver.dart
@@ -2,10 +2,28 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:crypto/crypto.dart';
+import 'package:hashlib/hashlib.dart';
+import 'package:hashlib/random.dart';
 
 String deriveMacOsKey() {
   var keyString = "${Platform.localHostname}-{Platform.environment['USER']}";
-  var key = sha256.convert(utf8.encode(keyString)).toString();
-  return key;
+  var key = pbkdf2(utf8.encode(keyString), utf8.encode(_getSalt()));
+  return key.hex();
+}
+
+String _getSalt() {
+  var saltFile = File("db/efs.salt");
+  if(!saltFile.existsSync()) {
+    saltFile.createSync(recursive: true);
+    var salt = _generateSalt();
+    saltFile.writeAsStringSync(salt);
+    return salt;
+  }
+
+  return saltFile.readAsStringSync();
+}
+
+String _generateSalt() {
+  var bytes = HashlibRandom.secure().nextBytes(16);
+  return base64Encode(bytes);
 }
