e80c1bb515ca720410c146b538985d011ceb9962
Jay Slater <slater.jay@gmail.com>
2026-02-06 15:24:36 -0500

    Add request/response content size to logger middleware

--- lib/server/middleware/logger_middleware.dart
+++ lib/server/middleware/logger_middleware.dart
@@ -13,8 +13,11 @@ Middleware createLoggerMiddleware() {
   return (Handler innerHandler) {
     return (request) async {
       var start = DateTime.now();
+      var requestSize = request.contentLength ?? 0;
       var response = await innerHandler(request);
-      _log.v('${request.method} ${request.requestedUri.path} - ${response.statusCode} - ${DateTime.now().difference(start).inMilliseconds}ms');
+      var duration = DateTime.now().difference(start).inMilliseconds;
+      var responseSize = response.contentLength ?? 0;
+      _log.v('${request.method} ${request.requestedUri.path} - ${response.statusCode} I:${requestSize} - O:${responseSize} - - ${duration}ms');
       return response;
     };
   };
