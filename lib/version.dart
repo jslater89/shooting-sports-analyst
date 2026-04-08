b1f1c6962cadc8c9b0615ef723bf58030b30b1d6
Jay Slater <slater.jay@gmail.com>
2026-04-07 21:18:33 -0400

    10.0.0-alpha2+30

--- lib/version.dart
+++ lib/version.dart
@@ -5,6 +5,6 @@
  */
 
 class VersionInfo {
-  static const String version = "10.0.0-alpha1";
-  static const int buildNumber = 29;
+  static const String version = "10.0.0-alpha2";
+  static const int buildNumber = 30;
 }
